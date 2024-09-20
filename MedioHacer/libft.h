b3b21816eba47342c093485f28b5ff6e2cd57a22
Rocio Femia Ruiz <rocfemia@c3r5s5.42madrid.com>
2024-09-20 12:34:20 +0200

    second

---
+++ MedioHacer/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rocfemia <rocfemia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 10:58:41 by rocfemia          #+#    #+#             */
+/*   Updated: 2024/09/20 11:33:38 by rocfemia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT.H
+#define LIBFT.H
+
+#include <stddef.h>
+
+int	ft_isalpha(int c)
+int	ft_isdigit(int c)
+int	ft_isalnum(int c)
+int	ft_isascii(int c)
+int	ft_isprint(int c)
+size_t	ft_strlen(const char *s)
+void	*ft_memset(void *s, int c, size_t n)
+void	ft_bzero(void *s, size_t n)
+void *ft_memcpy(void *dest, const void *src, size_t n)
+
+char	*ft_strchr(const char *s, int c)
+
+#endif
