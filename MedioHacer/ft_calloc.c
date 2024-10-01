652767e30ee211172f2dc750d89b32dac7814f10
Your Name <you@example.com>
2024-10-01 04:22:50 +0200

    eighteen

---
+++ MedioHacer/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 02:44:01 by roo               #+#    #+#             */
+/*   Updated: 2024/10/01 02:52:41 by roo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdint.h>   ????????
+
+void				*ft_calloc(size_t nmemb, size_t size)
+{
+    if (nmemb == SIZE_MAX && size == SIZE_MAX)
+        return(NULL);
+}
+#include <stdio.h>
+int main()
+{
+    printf("%s\n", ft_calloc( , ));
+    return(0);
+}
--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rocfemia <rocfemia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 21:38:00 by rocfemia          #+#    #+#             */
-/*   Updated: 2024/09/30 21:48:48 by rocfemia         ###   ########.fr       */
+/*   Updated: 2024/10/01 02:37:01 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,25 @@ int		ft_atoi(const char *nptr)
 	result = 0;
 	while ((nptr[i] == ' ') || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	while ((nptr[i] == '+') || (nptr[i] == '-'))
-	{
-		if (nptr[i] == '-')
-			sig = sig * -1;
+	if (nptr[i] == '-')
+		sig = sig * -1;
+	if ((nptr[i] == '+') || (nptr[i] == '-'))
 		i++;
-	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		result = result * 10 + nptr[i] - '0';
 		i++;
 	}
-	result = result * sig;
-	return (result);
+	return (result * sig);
 }
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 int	main(void)
 {
 	int	result1;
 	int	result2;
-	result1 = ft_atoi("	 -+--1182b8");
+	result1 = ft_atoi("  -2182b8");
 	printf("%i\n", result1);
-	result2 = atoi("	 -+--1182b8");
+	result2 = atoi("  -2182b8");
 	printf("%i\n", result2);
-}
+}*/
